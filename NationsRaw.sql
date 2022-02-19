54d4b8ad4ff92b23ac795d9227ea2c59fdd2080d
Niranjan Reddy Konudula <92642965+N-jan@users.noreply.github.com>
2022-02-19 13:03:22 +0530

    Create NationsRaw.sql

---
+++ NationsRaw.sql
@@ -0,0 +1,7 @@
+CREATE TABLE NationsRaw(
+Nation varchar(50),
+WTO_Status varchar(20),
+Per_Capita_GDP varchar(20),
+Trade_Deficit varchar(20),
+Fitch_Rating varchar(20),
+Fitch_Outlook vasrchar(5))
